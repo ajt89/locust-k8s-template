c0dc8c5103ba2101382ce65c1da6ac8ce821c012
AJ Tuquero <ajtuquero@gmail.com>
2019-03-23 23:06:43 -0400

    Simplify locust

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,64 @@
-from locust import HttpLocust
-
-from task_sets.task_set_a import TaskSetA
+from locust import HttpLocust, TaskSet
 
 
-class WebsiteUserA(HttpLocust):
-    task_set = TaskSetA
+class UserBehavior(TaskSet):
+
+    def index(self):
+        url = '/'
+        name = 'index'
+        catch_response = True
+
+        response = self.client.get(
+            url,
+            name=name,
+            catch_response=catch_response,
+        )
+
+        if 'index' in response.text:
+            response.success()
+        else:
+            response.failure('Got wrong response')
+
+    def route_a(self):
+        url = '/a'
+        name = 'route_a'
+        catch_response = True
+
+        response = self.client.get(
+            url,
+            name=name,
+            catch_response=catch_response,
+        )
+
+        if 'route_a' in response.text:
+            response.success()
+        else:
+            response.failure('Got wrong response')
+
+    def route_b(self):
+        url = '/b'
+        name = 'route_b'
+        catch_response = True
+
+        response = self.client.get(
+            url,
+            name=name,
+            catch_response=catch_response,
+        )
+
+        if 'route_b' in response.text:
+            response.success()
+        else:
+            response.failure('Got wrong response')
+
+    tasks = {
+        index: 1,
+        route_a: 2,
+        route_b: 1,
+    }
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
     min_wait = 1000
     max_wait = 1000
--- wrappers/http_client.py
+++
@@ -1,7 +0,0 @@
-class HTTPWrappers():
-
-    def __init__(self, client):
-        self.client = client
-
-    def get_request(self, url, name, headers):
-        return self.client.get(url, name=name, headers=headers, catch_response=True)
